0178e577b598a6ffc9b5ec5d675081de61177881
billazs <nabillaftrrr@gmail.com>
2025-04-14 11:32:55 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -2,8 +2,4 @@ function goToProfile() {
     window.location.href = "profile.html";
   }
   
-  function logout() {
-    alert("Anda telah logout.");
-    window.location.href = "index.html";
-  }
   
